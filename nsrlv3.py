e6b7831589ddb98de1d71361b1b006d5763d51af
galonias <erik.vestling@gmail.com>
2023-04-06 11:08:09 +0200

    first commit

--- nsrlv3.py
+++ nsrlv3.py
@@ -3,7 +3,7 @@ import csv
 import sqlite3
 import tempfile
 import zipfile
-import os.path
+import os
 
 parser = argparse.ArgumentParser(description='Extract unique MD5 and SHA1 combinations from an SQLite database inside a zip file.')
 parser.add_argument('zip_file', type=str, help='path to the zip file containing the SQLite database')
@@ -19,26 +19,32 @@ with zipfile.ZipFile(args.zip_file) as zip_file:
     if db_file_name is None:
         raise ValueError("No SQLite database file found in the zip file")
     with zip_file.open(db_file_name) as db_file:
-        # Create a temporary file to store the database contents
-        temp_file = tempfile.NamedTemporaryFile(delete=False)
-        temp_file.write(db_file.read())
-        temp_file.close()
+        # Create a temporary file next to the zip file to store the database contents
+        temp_file_name = os.path.join(os.path.dirname(args.zip_file), "temp.db")
+        with open(temp_file_name, 'wb') as temp_file:
+            chunk_size = 1024 * 1024  # 1 MB
+            while True:
+                chunk = db_file.read(chunk_size)
+                if not chunk:
+                    break
+                temp_file.write(chunk)
 
         # Connect to the SQLite database
-        conn = sqlite3.connect(temp_file.name)
+        conn = sqlite3.connect(temp_file_name)
         conn.execute("PRAGMA foreign_keys = ON")
         c = conn.cursor()
 
         # Retrieve unique combinations of md5 and sha1 values
         unique_values = set()
-        for row in c.execute("SELECT md5, sha1 FROM METADATA"):
+        for row in c.execute("SELECT md5, sha1 FROM FILE"):
             unique_values.add((row[0], row[1]))
 
         # Construct the output file name from the database file name
         output_file_name = os.path.splitext(os.path.basename(db_file_name))[0] + ".csv"
+        output_file_path = os.path.join(os.path.dirname(args.zip_file), output_file_name)
 
         # Write the unique combinations to the output CSV file
-        with open(output_file_name, "w", newline="") as csvfile:
+        with open(output_file_path, "w", newline="") as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(["MD5", "SHA1"])  # Write header row
             for md5, sha1 in unique_values:
@@ -47,4 +53,5 @@ with zipfile.ZipFile(args.zip_file) as zip_file:
         # Close the database connection and delete the temporary file
         c.close()
         conn.close()
-        temp_file.unlink()
+        os.remove(temp_file_name)
+        
