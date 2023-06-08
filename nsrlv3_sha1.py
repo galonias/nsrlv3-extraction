2d843b1ae48ebdb0eefe8e0e4f67052d186b58ef
galonias <33417979+galonias@users.noreply.github.com>
2023-06-08 12:00:44 +0200

    Updated to check whether the .db-file is already extracted and outputting the unique hashes into text-file instead

--- nsrlv3_sha1.py
+++ nsrlv3_sha1.py
@@ -18,6 +18,21 @@ with zipfile.ZipFile(args.zip_file) as zip_file:
             break
     if db_file_name is None:
         raise ValueError("No SQLite database file found in the zip file")
+
+    # Construct the output file name from the database file name
+    output_file_name = os.path.splitext(os.path.basename(db_file_name))[0] + ".txt"
+
+    # Determine the output file path
+    output_file_path = os.path.join(os.path.dirname(args.zip_file), output_file_name)
+
+    # Check if the output file already exists
+    if os.path.exists(output_file_path):
+        # Prompt the user for confirmation to overwrite the file
+        user_input = input("The output file already exists. Do you want to overwrite it? (y/n): ")
+        if user_input.lower() != "y":
+            print("Operation canceled. The existing file was not overwritten.")
+            exit()
+
     with zip_file.open(db_file_name) as db_file:
         # Create a temporary file next to the zip file to store the database contents
         temp_file_path = os.path.join(os.path.dirname(args.zip_file), os.path.splitext(os.path.basename(args.zip_file))[0] + ".db")
@@ -36,18 +51,13 @@ with zipfile.ZipFile(args.zip_file) as zip_file:
         c = conn.cursor()
 
         # Retrieve unique combinations of sha1 values
-        query = "SELECT sha1 FROM DISTINCT_HASH"
-        unique_values = (row for row in c.execute(query))
+        query = "SELECT DISTINCT sha1 FROM DISTINCT_HASH"
+        unique_values = (row[0] for row in c.execute(query))
 
-        # Construct the output file name from the database file name
-        output_file_name = os.path.splitext(os.path.basename(db_file_name))[0] + ".csv"
-
-        # Write the unique combinations to the output CSV file next to the zip file
-        output_file_path = os.path.join(os.path.dirname(args.zip_file), output_file_name)
-        with open(output_file_path, "w", newline="") as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(["SHA1"])  # Write header row
-            writer.writerows(unique_values)
+        # Write the unique combinations to the output text file next to the zip file
+        with open(output_file_path, "w") as txtfile:
+            for value in unique_values:
+                txtfile.write(str(value) + "\n")
 
         # Close the database connection and delete the temporary file
         c.close()
